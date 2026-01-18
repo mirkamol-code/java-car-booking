81c638a8d845e937b4542d811acbc41b9e7f7de0
mirkamol-code <mirkamol.m.a@gmail.com>
2026-01-18 19:27:03 +0500

    feat(models): added Car, CarBooking, User, Brand(enum)

---
+++ src/main/java/com/mirkamolcode/model/Car.java
@@ -0,0 +1,59 @@
+package com.mirkamolcode.model;
+
+import com.mirkamolcode.model.enums.Brand;
+
+public class Car {
+    private int regNumber;
+    private double rentalPricePerDay;
+    private Brand brand;
+    private boolean isElectric;
+
+    public Car(int regNumber, double rentalPricePerDay, Brand brand, boolean isElectric) {
+        this.regNumber = regNumber;
+        this.rentalPricePerDay = rentalPricePerDay;
+        this.brand = brand;
+        this.isElectric = isElectric;
+    }
+
+    public int getRegNumber() {
+        return regNumber;
+    }
+
+    public void setRegNumber(int regNumber) {
+        this.regNumber = regNumber;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+
+    public void setRentalPricePerDay(double rentalPricePerDay) {
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public boolean isElectric() {
+        return isElectric;
+    }
+
+    public void setElectric(boolean electric) {
+        isElectric = electric;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "regNumber=" + regNumber +
+                ", rentalPricePerDay=" + rentalPricePerDay +
+                ", brand=" + brand +
+                ", isElectric=" + isElectric +
+                '}';
+    }
+}
