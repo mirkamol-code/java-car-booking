f2e4bb98ec6380ba134b11b47c05528c56f4c4b1
Mirkamol <mirkamol.m.a@gmail.com>
2026-01-19 19:32:43 +0500

    feat(Menu): created Menu enum to add constants to store menu buttons and messages

---
+++ src/main/java/com/mirkamolcode/model/enums/Menu.java
@@ -0,0 +1,21 @@
+package com.mirkamolcode.model.enums;
+
+public enum Menu {
+    BOOK_CAR("1️⃣ - Book Car"),
+    VIEW_ALL_USER_BOOKED_CARS("2️⃣ - View All User Booked Cars"),
+    VIEW_ALL_BOOKINGS("3️⃣ - View All Bookings"),
+    VIEW_AVAILABLE_CARS("4️⃣ - View Available Cars"),
+    VIEW_AVAILABLE_ELECTRIC_CARS("5️⃣ - View Available Electric Cars"),
+    VIEW_ALL_USERS("6️⃣ - View all users"),
+    EXIT("7️⃣ - Exit");
+
+    private final String message;
+
+    Menu(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
