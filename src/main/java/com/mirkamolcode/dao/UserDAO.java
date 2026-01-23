56921c7da02ffceadc3a2e3bef13e870641bdb9d
Mirkamol <mirkamol.m.a@gmail.com>
2026-01-23 23:06:31 +0500

    fix(UserDAO): changed the class to the interface and added constants, abstract and default methods

--- src/main/java/com/mirkamolcode/dao/UserDAO.java
+++ src/main/java/com/mirkamolcode/dao/UserDAO.java
@@ -1,34 +1,39 @@
 package com.mirkamolcode.dao;
 
 import com.mirkamolcode.model.User;
-import com.mirkamolcode.model.enums.ResponseMessage;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import java.util.UUID;
 
-public class UserDAO {
-    private static User[] users;
+public interface UserDAO {
+    String FILE_PATH = "src/main/java/com/mirkamolcode/users.csv";
 
-    static {
-        users = new User[]{
-                new User("Ali"),
-                new User("Jamila"),
-                new User("Ensieh"),
-                new User("Yusuf")
-        };
+    User[] getAllUsers(File file);
+
+    User getUserById(UUID userId);
+
+    default boolean isUserExist(UUID uuid){
+        return getUserById(uuid) != null;
     }
 
-    public User[] selectAllUsers() {
-        return users;
-    }
+    default void writeUsersToFile(File file){
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            PrintWriter writer = new PrintWriter(fileWriter);
+            writer.println("7e4b9220-a47a-45a7-a33b-7182ee0dc30e, Leila");
+            writer.println("0236e9db-8c46-45a1-8fef-718d12e271f3, Bond");
+            writer.println("43bf7ab5-1f20-4693-a4f0-7319a7926d66, Ali");
+            writer.println("1fda7774-b948-42fa-ad35-7eb1a7248e35, Samira");
 
-    public User selectUserById(UUID userId) {
-        for (User user : users) {
-            if (user.getId().equals(userId)) {
-                return user;
-            }
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.getMessage();
         }
-        return null;
     }
-
 }
 
